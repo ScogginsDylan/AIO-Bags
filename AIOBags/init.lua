ce9a356cc2791c08b81f77690e6a9b2e48464ce1
Dylan Scoggins <dylanscoggins@gmail.com>
2023-03-10 12:03:47 -0600

    updated init file, issue getting widget to display

--- AIOBags/init.lua
+++ AIOBags/init.lua
@@ -3,13 +3,54 @@ local _, core = ...
 
 -- Custom Slash Commmands
 core.commands = {
-    [""] = core.Config.Toggle
+    ["show"] = core.Config.Toggle,
+
+    ["help"] = function()
+        print(" ")
+        core:Print("List of slash commands:")
+        core:Print("/at show|r - shows bag menu")
+        core:Print("/at help|r - shows help info")
+        print(" ")
+    end,
+
+    ["example"] = {
+        ["test"] = function(...)
+            core:Print("My Value:", tostringall(...))
+        end
+    }
 }
 
 -- Slash Command Handler
 local function HandleSlashCommands(str)
     if (#str == 0) then
-        core.commands.Toggle
+        core.commands.help()
+        return
+    end
+
+    local args = {}
+    for _, arg in ipairs({string.split(' ', str)}) do
+        if (#args > 0) then
+            table.insert(args, arg)
+        end
+    end
+
+    local path = core.commands
+
+    for id, arg in ipairs(args) do
+        if (#arg > 0) then
+            arg = arg:lower()
+            if (path[arg]) then
+                if (type(path[arg]) == "function") then
+                    path[arg](select(id + 1, unpack(args)))
+                    return
+                elseif (type(pathp[arg]) == "table") then
+                    path = path[arg]
+                end
+            else
+                core.commands.help()
+                return
+            end
+        end
     end
 end
 
